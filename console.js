3f481bfaf32f31756533abef3cb5fb5c2e174a0a
Michael Bianco <iloveitaly@gmail.com>
2019-05-11 13:17:06 -0600

    Adding basic console for easier testing
    Run with `node console.js`

---
+++ console.js
@@ -0,0 +1,13 @@
+var repl = require("repl");
+
+const Flight = require('./lib/bot/flight.js');
+const Alert = require('./lib/bot/alert.js');
+const mongoose = require('./lib/mongo.js');
+
+mongoose.connection.on('open', () => {
+  let replServer = repl.start('>');
+
+  replServer.context.Flight = Flight;
+  replServer.context.Alert = Alert;
+  replServer.context.mongoose = mongoose;
+});
